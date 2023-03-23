be8b8ab95954ef730242899e217f8658d8404b14
wafall <57603473+wafall@users.noreply.github.com>
2023-03-23 09:55:48 +0100

    siurek

--- Libruk/Libruk/Program.cs
+++ Libruk/Libruk/Program.cs
@@ -6,7 +6,7 @@ namespace Libruk
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");aaaa
         }
     }
 }
